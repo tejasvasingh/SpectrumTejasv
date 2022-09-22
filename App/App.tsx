4ed4c609dac8c929266b46b8b2569ae4e034973b
tejasvasingh <tejasvsingh.883@gmail.com>
2022-09-22 22:46:59 +0530

    done with natiuve bridging

--- App/App.tsx
+++ App/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppNavigation from './Navigations';
 import {Provider} from 'react-redux';
 import store from './Store/store';
+import DeviceInfo from 'react-native-device-info';
+import {Alert} from 'react-native';
 
+const isSimulator = async () => {
+  const value = await DeviceInfo.isEmulator();
+  if (value) {
+    Alert.alert('running on simulator');
+  }
+};
 const App = () => {
+  useEffect(() => {
+    isSimulator();
+  }, []);
   return (
     <Provider store={store}>
       <AppNavigation />
--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -5,10 +5,16 @@
 import 'react-native';
 import React from 'react';
 import App from '../App/App';
+import Buttons from '../App/Screens/Buttons';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+describe('TestCase for checking functionality of buttons', () => {
+  const tree = renderer.create(<Buttons />);
+  console.log(tree, 'ayuhs');
 });
